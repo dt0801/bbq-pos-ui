f4ec652cd4b8eda178695ec56348517066cbaa39
dt0801 <dinhtrong081001@gmail.com>
2026-03-04 09:36:20 +0700

    import css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';   // ← THÊM DÒNG NÀY
 import App from './App';
 import { AuthProvider } from './AuthContext';
 
